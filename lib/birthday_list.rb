e5b591cb3de0cd5b6b2162bd7c9ce73a65ad0ee8
Cristina Maria Ocana Manzano <cristinaocana1@gmail.com>
2018-09-06 22:56:48 +0100

    Save progress to birthdays exercise

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,23 @@
+require 'date'
 
+class BirthdayList
+  attr_reader :list
+  def initialize
+    @list = []
+  end
+
+  def add(name, birthday)
+    @list << {name: name, birthday: birthday }
+  end
+
+  def print_list
+    @list.each { |birthday| puts "#{birthday[:name]}'s birthday is on #{birthday[:birthday]} " }
+  end
+  def birthday_today
+    @list.each do |birthday|
+      if birthday[:birthday].day == Date.today.day && birthday[:birthday].month == Date.today.month
+        puts "It's #{birthday[:name]}'s birthday today!"
+      end
+    end
+  end
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,10 @@
+require "birthday_list"
 
+describe BirthdayList do
+   it { is_expected.to respond_to(:add).with(2).arguments}
+   it "stores birthdays in a list" do
+     birthdays = BirthdayList.new
+     expect(birthdays.add("David", "04/11/1979")).to eq [{name: "David", birthday: "04/11/1979"}]
+   end
+   it { is_expected.to respond_to(:print_list)}
+end
